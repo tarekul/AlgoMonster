b138bb3b2bab4ee7311415edbaa81f2a1199f587
tarekul <rahin6810@gmail.com>
2024-10-25 15:08:45 -0400

    insertion sort

---
+++ intro_to_sorting/insertion_sort.py
@@ -0,0 +1,9 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j] < arr[j - 1]:
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]
+            j -= 1
+    return arr
+
+print(insertion_sort([5, 3, 1, 2, 4]))
