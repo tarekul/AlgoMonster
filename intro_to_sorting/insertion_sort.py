9980d12e6379e71f1b450af16274afa1dc839dbd
tarekul <rahin6810@gmail.com>
2024-10-25 15:13:34 -0400

    add notes

--- intro_to_sorting/insertion_sort.py
+++ intro_to_sorting/insertion_sort.py
@@ -10,3 +10,10 @@ print(insertion_sort([5, 3, 1, 2, 4]))
 
 # space_complexity = O(1)
 # time_complexity = O(n^2)
+
+"""
+This is a stable algorithm because it preserves the relative order of equal elements. 
+Later elements will not be moved ahead of earlier equal elements.
+
+This is an in-place algorithm because it sorts the array in place and does not require additional memory for another array.
+"""
